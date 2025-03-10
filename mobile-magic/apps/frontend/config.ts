ae8620c4a72529fc934f3cae53ae52992355b58d
harkirat <harkirat96@gmail.com>
2025-03-10 09:50:51 +0530

    Prod

--- mobile-magic/apps/frontend/config.ts
+++ mobile-magic/apps/frontend/config.ts
@@ -1,5 +1,5 @@
 
-export const BACKEND_URL = process.env.NEXT_PUBLIC_BACKEND_URL || "http://localhost:9090";
+export const BACKEND_URL = process.env.NEXT_PUBLIC_BACKEND_URL || "https://primary-backend.antidevs.com";
 // TODO: This should come from the router when we add it eventually
 
-export const K8S_ORCHESTRATOR_URL = process.env.NEXT_PUBLIC_K8S_ORCHESTRATOR_URL || "http://localhost:7001";
+export const K8S_ORCHESTRATOR_URL = process.env.NEXT_PUBLIC_K8S_ORCHESTRATOR_URL || "https://k8s-orchestrator.antidevs.com";
